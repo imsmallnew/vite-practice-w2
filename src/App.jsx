a4cf4f87e9dfe85a349e3b7e7437e118ac109742
imsmallnew <imsmallnew@gmail.com>
2025-01-12 21:56:34 +0800

    React team work - Daneil adds w3, update layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'; // 引入 CSS
+// import 'bootstrap'; // 引入js
+import { Modal } from 'bootstrap';
+import Chart from 'chart.js/auto';
 import './App.css'
 
 function App() {
@@ -18,9 +21,63 @@ function App() {
     }
   ];
 
-  function showText (text) {
-    alert(text)
+  const [text, setText] = useState(null)
+  const [modalText, setModalText] = useState(null)
+  const [state, setState] = useState(false) // 避免按下open後二次觸發
+  
+  // 錯誤寫法 let myModal => useState更新渲染後失效會報錯 Uncaught TypeError: Cannot read properties of undefined (reading 'show')
+  const modalRef = useRef(null);
+  const modalMethodRef = useRef(null);
+
+  useEffect(() => {
+    modalMethodRef.current = new Modal(modalRef.current); 
+  }, [])
+
+  const openModal = (item) => {
+    setState(true);
+    setTimeout(()=>{
+      setModalText(item.author + " says: " + item.boxText)
+      // modalMethodRef.current.show() // 卡斯伯老師寫法
+      Modal.getInstance(modalRef.current).show(); // 助教教學寫法
+      // myModal.show() // 原始寫法
+      setState(false);
+    }, 500)
   }
+
+  const closeModal = () => {
+    modalMethodRef.current.hide()
+    // myModal.hide()
+  }
+
+  const acquisitionsRef = useRef(null);
+
+  useEffect(() => {
+    const data = [
+      { year: 2020, count: 10 },
+      { year: 2021, count: 20 },
+      { year: 2022, count: 15 },
+      { year: 2023, count: 25 },
+      { year: 2024, count: 22 },
+      { year: 2025, count: 30 },
+    ];
+  
+    new Chart(
+      acquisitionsRef.current,
+      {
+        type: 'bar',
+        data: {
+          labels: data.map(row => row.year),
+          datasets: [
+            {
+              label: 'Acquisitions by year',
+              data: data.map(row => row.count)
+            }
+          ]
+        }
+      }
+    );
+  }, []);
+   
 
   return (
     <>
@@ -29,30 +86,70 @@ function App() {
             <div className="container-fluid">
             <form className="container-fluid justify-content-center">
               <div className="nav justify-content-center">
-                <h5>React Team Work - Group6 - W2</h5>
+                <h5 className='text-primary'>React Team Work - Group6 - W2,W3</h5>
               </div>
             </form>
           </div>
         </nav>
       </div>
+      
       <div className="container mt-3">
+        <div className="mb-3">
+          搜尋: <input type="text" onChange={(e) => {
+            setText(e.target.value)
+          }}></input>
+          {text}
+        </div>
         <div className="row">
         {data.map((item, index)=> {
           return(
-        <div className="card col-md-4" key={index}>
-          <div className="subWrap">
-              <div className="imgFrame mt-3">
-                  <img src={item.imgUrl} className="card-img w-100 h-100 text-start object-fit-cover" alt="副圖" />
+        <div className="col-md-4 col-12" key={index}>
+          <div className='card'>
+            <div className="subWrap">
+                <div className="imgFrame">
+                    <img src={item.imgUrl} className="card-img-top object-fit-cover" alt="副圖" />
+                </div>
+            </div>
+            <div className="card-body">
+              <h5 className="card-title">{item.author}</h5>
+              <p className="card-text">{item.content}</p>
+              <div className='row row-cols-2 d-flex justify-content-between'>
+                  <div className='col'>
+                    <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">
+                      原始Modal
+                    </button>
+                  </div>
+                  <div className='col'>
+                    <button type="button" className="btn btn-success float-end" onClick={() => { openModal(item) }} disabled={state}>
+                      JS觸發Modal
+                    </button>
+                  </div>
               </div>
-          </div>
-          <div className="card-body">
-            <h5 className="card-title">{item.author}</h5>
-            <p className="card-text">{item.content}</p>
-            <button type="button" onClick={()=> showText(item.boxText)} className="btn btn-primary">Show Text</button>
+            </div>
           </div>
         </div>)
         }
         )} 
+        </div>
+        {/***  Char ***/}
+        <canvas ref={acquisitionsRef} className='mt-5 mb-5'/>
+        {/***  Modal ***/}
+        <div className="modal fade" id="myModal" ref={modalRef} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+                {modalText}
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" className="btn btn-secondary" onClick={() => { closeModal() }}>JS Close</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div> 
     </>
